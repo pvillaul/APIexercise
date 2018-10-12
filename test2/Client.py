2fa3f90f17bc5694ed57361e9870bdabafbce2cc
Meyer <meyermontagner@gmail.com>
2018-10-12 16:21:47 +0200

    Comentarios y detalles
    Agregue comentarios a client.py y a server.py. Eliminé de server.py un
    comentario y una función que no estaban en uso. Cambié el número que
    reciben las funciones de calculate, para que pueda ser un número real.

--- test2/Client.py
+++ test2/Client.py
@@ -1,17 +1,33 @@
-import requests
-import json
+# Authors: Meyer Montagner, Pablo Villalba
+# Programa que funciona como cliente al servidor. Usado como práctica del concepto de API.
 
-result = requests.get('http://127.0.0.1:5000/sayhello')
+import requests
+
+""" Acceso a endpoint /sayhello/. 
+Retorna un JSON con un saludo. """
+result = requests.get('http://127.0.0.1:5000/sayhello')  # type: requests.Response
 print(result.text)
 
-result2 = requests.get('http://127.0.0.1:5000/calculate/9')
+""" Acceso a endpoint /calculate/<num>. 
+Recibe un número en la ruta.
+Retorna un JSON con el cuadrado del número. """
+result2 = requests.get('http://127.0.0.1:5000/calculate/3.2')  # type: requests.Response
 print(result2.text)
 
-result3 = requests.get('http://127.0.0.1:5000/calculate/?num=3')
+""" Acceso a endpoint /calculate/. 
+Recibe un número como parámetro.
+Retorna un JSON con el cuadrado del número. """
+result3 = requests.get('http://127.0.0.1:5000/calculate/?num=3.45')  # type: requests.Response
 print(result3.text)
 
-result4 = requests.get('http://127.0.0.1:5000/concat/?cad1=Alberto&cad2=Fernandez')
+""" Acceso a endpoint /concatenate/. 
+Recibe dos cadenas de caracteres como parámetros.
+Retorna un JSON con las dos cadenas concatenadas. """
+result4 = requests.get('http://127.0.0.1:5000/concatenate/?string1=Alberto&string2=Fernandez') # type: requests.Response
 print(result4.text)
 
-result5 = requests.get('http://127.0.0.1:5000/users/1')
+""" Acceso a endpoint /users/<id>. 
+Recibe un número ID en la ruta.
+Retorna un JSON que contiene el nombre del usuario correspondiente al ID. """
+result5 = requests.get('http://127.0.0.1:5000/users/1')  # type: requests.Response
 print(result5.text)
--- test2/Server.py
+++ test2/Server.py
@@ -1,46 +1,59 @@
-from flask import Flask,jsonify,request
+# Authors: Meyer Montagner, Pablo Villalba
+# Programa que inicia un servidor usado como práctica del concepto de API.
 
-app = Flask(__name__)
-# WS de respuesta en directorio raiz.
+from typing import Dict
+from flask import Flask, jsonify, request
 
-'''akkakakaakakkakakak
-kakakaakakakakkakakaka
-hahahahakikakakakakaka'''
+app = Flask(__name__)                           # type: Flask
 
-dic ={'1' : 'Paco', '2' : 'Pepe'}
-@app.route('/users/<id>',methods=['GET'])
-def get_user(id):
-    user = dic[id]
+userIdDictionary = {'1': 'Paco', '2': 'Pepe'}   # type: Dict[str, str]
+
+
+@app.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """Recibe desde la ruta el id de un usuario.
+    Retorna un JSON con el usuario registrado con el id recibido."""
+    user = userIdDictionary[user_id]  # type: str
     return jsonify(user)
 
-@app.route('/concat/',methods=['GET'])
-def concat():
-    cad1 = request.args.get('cad1')
-    cad2 = request.args.get('cad2')
-    result = cad1 + cad2
+
+@app.route('/concatenate/', methods=['GET'])
+def concatenate():
+    """Recibe por parámetros dos cadenas de caracteres.
+    Retorna un JSON con las dos cadenas concatenadas."""
+    string1 = request.args.get('string1')   # type: str
+    string2 = request.args.get('string2')   # type: str
+    result = string1 + string2              # type: str
     return jsonify(result)
 
-@app.route('/calculate/<num>',methods=['GET'])
-def calcular_sqr(num):
-    num = int(num)
-    num = num * num
-    return jsonify(num)
 
-@app.route('/calculate/',methods=['GET'])
-def calcular_param():
-    num = request.args.get('num')
-    num = int(num)
-    result = num * num
+@app.route('/calculate/<num>', methods=['GET'])
+def calculate_square(num):
+    """Recibe desde la ruta un número.
+    Retorna un JSON con el cuadrado del número recibido."""
+    num = float(num)          # type: float
+    result = num * num        # type: float
     return jsonify(result)
 
-@app.route('/sayhello',methods=['GET'])
-def saludar():
-    var = 'hello'
+
+@app.route('/calculate/', methods=['GET'])
+def calculate_square_with_param():
+    """Recibe como parámetro un número.
+    Retorna un JSON con el cuadrado del número recibido."""
+    num = request.args.get('num')   # type: str
+    num = float(num)                # type: float
+    result = num * num              # type: float
+    return jsonify(result)
+
+
+@app.route('/sayhello', methods=['GET'])
+def say_hello():
+    """Retorna un JSON con un string de saludo.
+    No recibe parámetros."""
+    var = 'hola'                    # type: str
     return jsonify(var)
 
-@app.route('/',methods=['GET'])
-def index():
-    return jsonify({'URJC University' : 'API Testing'})
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    """Inicia la ejecución del servidor."""
+    app.run(host='127.0.0.1', port=5000, debug=True)
